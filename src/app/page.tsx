1c2427016705666e2f1832d2f64339f62016483d
gonzaemon111 <goki14914@icloud.com>
2023-11-26 17:02:08 +0900

    🛡️ Auth0の導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { TopPage } from '@/components/pages/TopPage';
+
 export default function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <p>Wikimap</p>
-    </main>
-  );
+  return <TopPage />;
 }
